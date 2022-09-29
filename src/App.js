40ff5f08bf9b04c532a55ec4e29216f694176e6c
Hamza Ilyas <hamza24061@gmail.com>
2022-09-30 01:58:27 +0500

    Error's Resolved

--- src/App.js
+++ src/App.js
@@ -1,8 +1,5 @@
 import AppRouter from './app-router/AppRouter';
 import './App.css';
-import Login from './pages/Login';
-import Signup from './pages/Signup';
-import Todo from './pages/Todo';
 
 function App() {
   return (
